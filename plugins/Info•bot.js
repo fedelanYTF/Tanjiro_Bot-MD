6ba30c673ec44294749791d849392dbae5f15351
FedelanYT <federicovigevani999@gmail.com>
2025-04-21 17:13:11 -0300

    Update Info•bot.js

--- plugins/Info•bot.js
+++ plugins/Info•bot.js
@@ -10,7 +10,7 @@ conn.reply(m.chat, `Esta bien, agradezco tu sinceridad por haberme respondido, t
 
 if (chat.isBaneed) return
 if (/^reglas$/i.test(m.text)) {
-conn.reply(m.chat, `*Hola si quieres unir a la bot deberas seguir las reglas basicas* 👇\n\n> No hacer spam a la bot\n\n> No llamar a la bot\n\n> El grupo debe de tener un minimo de 25 miembros o no sera otorgado el bot\n\n> No añadir a la bot sin permisos del creador \n\n> Wa.me/5493876639332\n\n> Disfrute de *${botname}Bot-MD Powered By Ianalejandrook15x*`, m, )
+conn.reply(m.chat, `*Hola si quieres unir a la bot deberas seguir las reglas basicas* 👇\n\n> No hacer spam a la bot\n\n> No llamar a la bot\n\n> El grupo debe de tener un minimo de 25 miembros o no sera otorgado el bot\n\n> No añadir a la bot sin permisos del creador \n\n> Wa.me/5491156178758\n\n> Disfrute de *${botname}Bot-MD Powered By fedelanYT*`, m, )
 }
   
 if (/^Si Te perdono$/i.test(m.text)) {
